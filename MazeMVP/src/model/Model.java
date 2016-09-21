fc5e49b9d3bcb6d5c509f1b67b823a6388e41024
osher_000 <osher_000@Osher>
2016-09-21 20:04:28 +0300

    fix

--- MazeMVP/src/model/Model.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-import algorithms.mazeGenerators.Maze3d;
-
-public interface Model {
-	void generateMaze(String name, int rows, int cols);
-	Maze3d getMaze(String name);
-	void exit();	
-}
--- MazeMVP/src/presenter/Command.java
+++
@@ -1,5 +0,0 @@
-package presenter;
-
-public interface Command {
-	void doCommand(String[] args);
-}
